f793d56b751b65ac30e09ad76ae94dfd221b58a0
joao <jgmachado@inf.ufsm.br>
2020-12-03 13:30:56 -0300

    [FEAT] Basic player3D controller, move, look and jump

---
+++ Assets/Player3D/Movement/PlayerLookAround.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLookAround : MonoBehaviour
+{
+
+    public float mouseSensitivity = 100f;
+    public Transform playerBody;
+
+    float _xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+            LockOrUnlockCursor();
+        Player3DLookAround();
+    }
+
+    private void Player3DLookAround()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        _xRotation -= mouseY;
+        _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(_xRotation, 0, 0);
+
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+
+    private static void LockOrUnlockCursor()
+    {  
+            if (Cursor.lockState == CursorLockMode.None)
+                Cursor.lockState = CursorLockMode.Locked;
+            else
+                Cursor.lockState = CursorLockMode.None;       
+    }
+}
---
+++ Assets/Player3D/Movement/PlayerMovement.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //References--------------------------
+    public CharacterController controller;    
+
+    public float speed = 12f;
+    public float jumpHeight = 3f;
+
+    //GroundCheckReferences
+    public Transform groundCheck;
+    public float groundDistance;
+    public LayerMask groundMask;
+    
+   //-------------------------------------
+
+    Vector3 _velocity;
+    bool _isGrounded;
+
+    //This will be removed soon
+    public float gravity = -9.81f;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GroundCheck();
+        PlayerMove();
+
+        if(Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+            _velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        
+
+        ApplyGravity();
+    }
+
+    private void ApplyGravity()
+    {
+        _velocity.y += gravity * Time.deltaTime;
+        controller.Move(_velocity * Time.deltaTime);
+    }
+
+    private void GroundCheck()
+    {
+        _isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (_isGrounded && _velocity.y < 0)
+        {
+            _velocity.y = -2f;
+        }
+    }
+
+    private void PlayerMove()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
