4629b532baff9938617cb2dd2086a1bdac1fa3fb
joao <jgmachado@inf.ufsm.br>
2020-12-03 20:20:33 -0300

    [FEAT] 2d First Movements

---
+++ Assets/RotateCrazy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCrazy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float xSpeed, ySpeed, zSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.Rotate(xSpeed * Time.deltaTime, ySpeed * Time.deltaTime, zSpeed * Time.deltaTime);
+    }
+}
