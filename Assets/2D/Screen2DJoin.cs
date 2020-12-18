75c19fc273150fc34cd00396745e52faac3d4d8d
joao <jgmachado@inf.ufsm.br>
2020-12-18 00:33:36 -0300

    [feat] nova forma de verificar parede

--- Assets/2D/Screen2DJoin.cs
+++ Assets/2D/Screen2DJoin.cs
@@ -20,7 +20,21 @@ public class Screen2DJoin : MonoBehaviour
     {
         if (currentJoinScreen)
         {
-            if (Vector3.Dot(transform.forward, other.transform.forward) > Vector3.Dot(transform.forward, currentJoinScreen.forward))
+            var interacting = IsIntersecting(transform, other.transform);
+            Debug.Log("interacting = " + interacting);
+
+            //var dotTransformOther = Vector3.Dot(transform.forward, other.transform.forward);
+            //var dotTransformCurrent = Vector3.Dot(transform.forward, currentJoinScreen.forward);
+
+            //Debug.Log(other.gameObject + "TransformWithOther = " + dotTransformOther);
+            // Debug.Log(currentJoinScreen.gameObject + "TransformCurrent = " + dotTransformCurrent);
+
+            //if (dotTransformOther > dotTransformCurrent)
+            // {
+
+            //   UpdateCurrentJoinScreen(other);
+            //}
+            if (interacting)
             {
                 UpdateCurrentJoinScreen(other);
             }
@@ -36,6 +50,43 @@ public class Screen2DJoin : MonoBehaviour
         Screen2DJoin otherScreen = other.GetComponent<Screen2DJoin>();
         otherScreen.currentJoinScreen = transform;
         currentJoinScreen = otherScreen.transform;
+    }
+
+
+    bool IsIntersecting(Transform self, Transform other)
+    {
+
+        Transform L1_start = self;
+        Vector3 L1_end = self.forward * 90;
+        Transform L2_start = other;
+        Vector3 L2_end = other.forward * 90;
+
+
+        bool isIntersecting = false;
+
+        //3d -> 2d
+        Vector2 p1 = new Vector2(L1_start.position.x, L1_start.position.z);
+        Vector2 p2 = new Vector2(L1_end.x, L1_end.z);
+
+        Vector2 p3 = new Vector2(L2_start.position.x, L2_start.position.z);
+        Vector2 p4 = new Vector2(L2_end.x, L2_end.z);
+
+        float denominator = (p4.y - p3.y) * (p2.x - p1.x) - (p4.x - p3.x) * (p2.y - p1.y);
+
+        //Make sure the denominator is > 0, if so the lines are parallel
+        if (denominator != 0)
+        {
+            float u_a = ((p4.x - p3.x) * (p1.y - p3.y) - (p4.y - p3.y) * (p1.x - p3.x)) / denominator;
+            float u_b = ((p2.x - p1.x) * (p1.y - p3.y) - (p2.y - p1.y) * (p1.x - p3.x)) / denominator;
+
+            //Is intersecting if u_a and u_b are between 0 and 1
+            if (u_a >= 0 && u_a <= 1 && u_b >= 0 && u_b <= 1)
+            {
+                isIntersecting = true;
+            }
+        }
+
+        return isIntersecting;
     }
 
     private void OnTriggerExit(Collider other)
