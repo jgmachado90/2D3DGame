40b721e96686d823b30a1ad57a8922b9da2384e0
joao <jgmachado@inf.ufsm.br>
2021-01-08 20:27:44 -0300

    de volta

---
+++ Assets/PlatformOneDirection.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformOneDirection : MonoBehaviour
+{
+    public float speed;
+    bool moving;
+    public Transform target;
+    private void Start()
+    {
+        moving = true;
+    }
+
+    private void Update()
+    {
+        if(moving)
+            transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y, transform.position.z);
+        if (Vector3.Distance(transform.position, target.position) < 0.01f)
+            moving = false;
+    }
+}
